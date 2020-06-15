d4c5d028d0a2597514a9b99a6d461064f79f4c8a
Baiazitov <ivbayazitov@yandex.ru>
2020-06-15 23:54:37 +0300

    Third try

--- posts/views.py
+++ posts/views.py
@@ -44,7 +44,7 @@ def new_post(request):
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    post_list = Post.objects.filter(author=author)
+    post_list = author.posts.all()
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number) 
